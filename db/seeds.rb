0ceb51ddb5f66a7b973d28330e62c05e206c85ef
Batou99 <lorenzo.lopez@pam-it.com>
2016-07-30 10:21:06 +0000

    Add seed data and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+UiControl.delete_all
+slider_ui = UiControl.create(kind: 'slider')
+button_ui = UiControl.create(kind: 'button')
+select_ui = UiControl.create(kind: 'select')
+
+Control.delete_all
+volume_slider     = Control.create(display_name: 'Volume', ui_control: slider_ui)
+brightness_slider = Control.create(display_name: 'Brightness', ui_control: slider_ui)
+power_button      = Control.create(display_name: 'Power', ui_control: button_ui)
+playlist_select   = Control.create(display_name: 'Playlist', ui_control: select_ui)
+
+DeviceType.delete_all
+samsung_audio = DeviceType.create(name: 'Samsung Audio', controls: [power_button, volume_slider, playlist_select])
+sony_audio    = DeviceType.create(name: 'Sony Audio', controls: [power_button, playlist_select])
+apple_tv      = DeviceType.create(name: 'Apple TV Video', controls: [volume_slider, brightness_slider, power_button])
+
+Device.delete_all
+Device.create(name: 'Front Audio', ip_address: '192.168.1.10', device_type: sony_audio)
+Device.create(name: 'Rear Audio',  ip_address: '192.168.1.11', device_type: samsung_audio)
+Device.create(name: 'Apple TV',    ip_address: '192.168.1.12', device_type: apple_tv)
